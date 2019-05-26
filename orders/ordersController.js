a7063ac93e7f2517edd442178516b6b1755c1135
prabakargr <prabakar1993@gmail.com>
2019-05-26 15:30:12 +0530

    changes

--- orders/ordersController.js
+++ orders/ordersController.js
@@ -14,10 +14,15 @@ var remove=function(req,res){
         status:"Removed successfully"
     })
 }
+var list=function(req,res){
+    var data=[{"id":"1","productname":"boost ball","quantity":"10","orderdate":"2019-05-26","price":"30","option":"MRF","ExpectedDeliveryDate":"2019-05-27"},{"id":"2","productname":"stumber ball","quantity":"20","orderdate":"2019-05-26","price":"35","option":"MRF","ExpectedDeliveryDate":"2019-05-26"},{"id":"3","productname":"stumps","quantity":"10","orderdate":"2019-05-26","price":"120","option":"MRF","ExpectedDeliveryDate":"2019-05-28"},{"id":"4","productname":"Gloves","quantity":"40","orderdate":"2019-05-26","price":"250","option":"BridgeStone","ExpectedDeliveryDate":"2019-05-30"}]
+    res.send({data:data})
+}
 
 
 module.exports={
     create,
     update,
-    remove
+    remove,
+    list
 }
--- orders/oredersRouting.js
+++ orders/oredersRouting.js
@@ -5,5 +5,6 @@ var orderRouting=express.Router()
 orderRouting.route('/create').post(orderController.create)
 orderRouting.route('/update').post(orderController.update)
 orderRouting.route('/remove').post(orderController.remove)
+orderRouting.route('/list').get(orderController.list)
 
 module.exports=orderRouting
