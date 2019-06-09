91cb3f559b79737732a28f79b72c58e56b3433f7
prabakargr <prabakar1993@gmail.com>
2019-06-09 10:27:23 +0530

    list added

--- invoice/invoiceController.js
+++ invoice/invoiceController.js
@@ -14,10 +14,18 @@ var remove=function(req,res){
         status:"Removed successfully"
     })
 }
+var list=function(req,res){
+
+    var data=[{"id":"1","orderid":"5376377","invoicedate":"2019-06-08","amount":"3876"},{"id":"2","orderid":"abd5372","invoicedate":"2019-06-09","amount":"89775"},{"id":"3","orderid":"354x34b","invoicedate":"2019-06-08","amount":"1876"},{"id":"4","orderid":"6743ab","invoicedate":"2019-06-08","amount":"5634"}]
+    res.send({
+        data:data
+    })
+}
 
 
 module.exports={
     create,
     update,
-    remove
+    remove,
+    list
 }
